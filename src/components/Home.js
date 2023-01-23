40320b7c72ad5dce6fe6163e5aaa6e791bbea260
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-24 00:49:10 +0600

    Delete from db & update display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+
+    const [displayUsers, setDisplayUsers] = useState(users);
 
     const handleDelete = user => {
         const agree = window.confirm(`Are you sure you want to delete: ${user.name}`)
@@ -13,15 +15,26 @@ const Home = () => {
                 method: 'DELETE'
             })
                 .then(res => res.json())
-                .then(data => console.log(data));
+                .then(data => {
+                    console.log(data)
+
+                    if (data.deletedCount > 0) {
+                        alert('User deleted successfully');
+
+                        // user delete hobar por display te jotogulo user obosisto ase tader dekhao, allUsr er joto id ase tar konotar sathe (user._id) delete kora id ta milbe na, tar mane oita show o hobe na
+                        const remainingUsers = 
+                        displayUsers.filter(allUsr => allUsr._id !== user._id);
+                        setDisplayUsers(remainingUsers);
+                    }
+                });
         }
     }
     return (
         <div>
-            <h2>Users: {users.length}</h2>
+            <h2>Users: {displayUsers.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>
+                    displayUsers.map(user => <p key={user._id}>
                         {user.name} {user.address} {user.email} <button onClick={() => handleDelete(user)}>X</button>
                     </p>)
                 }
