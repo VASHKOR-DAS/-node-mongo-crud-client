76660c6f5102bcadf18626fda26debbf8b4f8529
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-23 20:55:09 +0600

    Read from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
     return (
         <div>
-            <h2>Users: </h2>
+            <h2>Users: {users.length}</h2>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>
+                        {user.name} {user.address} {user.email}
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
