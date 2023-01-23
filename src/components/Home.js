e88fc01b7c7b6187c7aa30712d2e6a7d57879295
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-23 21:14:58 +0600

    Delete krr age user er kas theke confirmation nite chacci

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,21 @@ import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+
+    const handleDelete = user => {
+        const agree = window.confirm(`Are you sure you want to delete: ${user.name}`)
+        // console.log(agree);
+        if (agree) {
+            console.log('user dlt id:', user._id);
+        }
+    }
     return (
         <div>
             <h2>Users: {users.length}</h2>
             <div>
                 {
                     users.map(user => <p key={user._id}>
-                        {user.name} {user.address} {user.email}
+                        {user.name} {user.address} {user.email} <button onClick={() => handleDelete(user)}>X</button>
                     </p>)
                 }
             </div>
