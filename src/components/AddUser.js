41250597f726e6c9282b32f0b4cfc6bc7cadf5c9
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-23 16:59:22 +0600

    multiple field add krr easy way

---
+++ src/components/AddUser.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const AddUser = () => {
+
+    const [user, setUser] = useState({});
+    const handleAddUser = event => {
+        event.preventDefault();
+        // const form = event.target;
+        // const name = form.name.value;
+        // const email = form.email.value;
+        console.log(user);
+    }
+
+    // multiple field add krr easy way
+    const handleInputBlur = event => {
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = { ...user } // user state a ja ase sob copy korlam
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+
+    return (
+        <div>
+            <h2>Add User</h2>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type="text" name="name" placeholder='name' required />
+                <br />
+                <input onBlur={handleInputBlur} type="email" name="email" placeholder='email' required />
+                <br />
+                <button type="submit">Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
